55b4d01fc82161e1267bd4fe28207b028dd65577
Sharvil Palvekar <sharvil.palvekar@gmail.com>
2025-02-15 17:33:00 +0530

    v1.8

--- src/app/footer.js
+++ src/app/footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-red-900  text-white text-center py-4 mt-6 h-[4vw] shadow-lg sticky bottom-0">
+    <footer className="bg-red-900  text-white text-[1.3vw] text-center py-4 h-[4vw] shadow-lg sticky bottom-0">
       <p>
         For more information, visit our{" "}
-        <span className="hover:text-lg transition-all ease-in-out"><a href="https://dharmawiki.org/index.php/Main_Page" className="text-blue-400 ">
+        <span className="hover:text-[1.5vw] transition-all ease-in-out"><a href="https://dharmawiki.org/index.php/Main_Page" className="text-blue-400 ">
         Dharma Wiki
         </a></span >.
       </p>
